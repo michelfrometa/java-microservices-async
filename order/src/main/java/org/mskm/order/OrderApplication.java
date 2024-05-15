b71971e7c1338bcbbaa9fc02f78aa3981691b5d0
Michel Lazaro Frometa Burey <michel@linkapital.com.br>
2024-05-15 07:00:55 -0300

    Creating Inventory and Order Services

---
+++ order/src/main/java/org/mskm/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package org.mskm.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+}
